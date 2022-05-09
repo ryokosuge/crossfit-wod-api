19b8d0a3c2768a2f3399a08f6537ed9079f5bfed
ryokosuge <ryo.kosuge@gmail.com>
2022-05-09 18:24:57 +0900

    feat:	Group associated resources together (logical nesting)

---
+++ src/controllers/recordController.js
@@ -0,0 +1,36 @@
+const recordService = require("../services/recordService");
+
+const getRecordsForWorkout = (req, res) => {
+	const {
+		params: { workoutId },
+	} = req;
+	if (!workoutId) {
+		res
+			.status(400)
+			.send({
+				status: "FAILED",
+				data: {
+					error: "Parameter ':workoutId' can not be empty"
+				},
+			});
+		return;
+	}
+
+	try {
+		const records = recordService.getRecordForWorkout(workoutId);
+		res.send({ status: "OK", data: records })
+	} catch (error) {
+		res
+      .status(error?.status || 500)
+      .send({
+				status: "FAILED",
+				data: {
+					error: error?.message || error
+				},
+			});
+	}
+};
+
+module.exports = {
+	getRecordsForWorkout,
+};
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,9 +1,11 @@
 const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", workoutController.getAllWorkouts);
 router.get("/:workoutId", workoutController.getOneWorkouts);
+router.get("/:workoutId/records", recordController.getRecordsForWorkout);
 router.post("/", workoutController.createNewWorkout);
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 router.delete("/:workoutId", workoutController.deleteOneWorkout);
