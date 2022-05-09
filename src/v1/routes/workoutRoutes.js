605a133d5252f7c79a55387d99e5c897b2fdb5b3
ryokosuge <ryo.kosuge@gmail.com>
2022-05-09 21:48:30 +0900

    feat:	Document your API properly

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -3,6 +3,44 @@ const recordController = require("../../controllers/recordController");
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.get("/", workoutController.getAllWorkouts);
 router.get("/:workoutId", workoutController.getOneWorkouts);
 router.get("/:workoutId/records", recordController.getRecordsForWorkout);
